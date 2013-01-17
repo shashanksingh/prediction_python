63d9c265beb05a9f3b89c8e215b4fab7351ce8ae
shashank singh <shashank.personal@gmail.com>
2013-01-17 18:19:01 +0530

    scatter plot is being created but of a random value

--- python/main.py
+++ python/main.py
@@ -1,11 +1,12 @@
 from queries import *
 from download import *
-
+from plot_scatter_graph import *
 
 def run():
 	print "executing"
 	data=execute(get_all_pickup_drop_lat_lng_timestamp())
 	save_to_backend(data,"file")	
+	scatter_plot()
 
 
 run()
