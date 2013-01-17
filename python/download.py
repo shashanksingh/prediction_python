2f96a6db9293e6cd34f9edb1b3b6e8273d70c5b8
shashank singh <shashank@olacabs.com>
2013-01-17 15:31:02 +0530

    now we have a query for gettitng all pickup and drop address

--- python/download.py
+++ python/download.py
@@ -4,31 +4,23 @@
 import _mysql
 import sys
 from queries import *
-from phpserialize import *
 
 
 
 con = None
 
-try:
-
-    con = _mysql.connect('localhost', 'root', 
-        'root', 'blackbox_prod')
-        
-    con.query(get_all_p2p_metadata())
-    result = con.use_result()
+def execute(query):
+	try:
+    	con = _mysql.connect('localhost', 'root','root', 'blackbox_prod')
+    	con.query(query)
+    	result = con.use_result()
+    	full_result_query=result.fetch_all()
     
-    a=result.fetch_all()
-    print a[0][0]
+	except _mysql.Error, e:
+    	return "Error %d: %s" % (e.args[0], e.args[1])
 
-    loads(a[0][0])	
-    
-except _mysql.Error, e:
-  
-    print "Error %d: %s" % (e.args[0], e.args[1])
-    sys.exit(1)
+	finally:
+    	if con:
+        	con.close()
 
-finally:
-    
-    if con:
-        con.close()
+	return full_result_query
--- python/queries.py
+++ python/queries.py
@@ -4,3 +4,26 @@ def get_all_p2p_metadata():
 
 def get_all_p2p_metadata_for_hours(hour):
 	return "select p2p_metadata as total_city_taxi_bookings from ola_bookings where service_type='p2p' and year(pickup_date)=2012 and hour(pickup_date)="+hour
+
+def get_all_pickup_drop_lat_lng_timestamp():
+	"""instead of reading ola_Bookings table this reads from ola_citytaxi_Status_udapte table which get update from driver app"""
+	return 
+	"""	select ob.id,
+		group_concat(nst.status),
+		group_concat(nst.lat) lat,
+		group_concat(nst.lng) lng,
+		group_concat(nst.timestamp) timestamp
+		from
+			ola_bookings ob
+		left join
+			ola_citytaxi_status_update nst ON ob.id = nst.booking_id
+		where
+		convert_tz(ob.pickup_date, '+0:00', '+5:30') between '2013-1-1 00:00:00' and '2013-1-15 23:59:59'
+		and ob.status = 'completed'
+		and ob.service_type = 'p2p'
+		and ob.service_city = 'Mumbai'
+		and ob.deleted = 0
+		and nst.status in (5 , 6)
+		group by ob.id
+		order by nst.status ASC;
+	"""
