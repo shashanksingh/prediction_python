3dce25fbf18cd06d6777fef615820c3fe7c839b9
shashank singh <shashank.personal@gmail.com>
2013-01-18 01:10:35 +0530

    plots are happening , yay :)

--- python/main.py
+++ python/main.py
@@ -6,6 +6,7 @@ def run():
 	print "executing"
 	#data=execute(get_all_pickup_drop_lat_lng_timestamp())
 	data=execute(get_all_pickup_drop_lat_lng_timestamp_moved_origin())
+	len(data)
 	#save_to_backend(data,"file")	
 	scatter_plot(data)
 
