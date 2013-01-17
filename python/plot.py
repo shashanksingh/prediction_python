f2d0c4aa677fb28ed92067b163cac2802154fd18
shashank singh <shashank@olacabs.com>
2013-01-17 20:50:44 +0530

    wtf is ValueError: zero-size array to maximum.reduce without identit??

--- python/plot.py
+++ python/plot.py
@@ -2,36 +2,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import NullFormatter
 
-_x = []
-_y = []
+x = []
+y = []
+
 def convert_to_numpy_x_y(data):
+	_x = []
+	_y = []
 	for booking in data:
 		if (booking[2] != None and booking[3] != None):
 			try:
 				lats = booking[2].split(",")
 			except ValueError, RuntimeError:
-				continue
+				lat = ['0.00','0.00']
 			try:
 				lngs = booking[3].split(",")
 			except ValueError , RuntimeError:
-				continue
+				lngs = ['0.00','0.00']
 		
-			_x.append(lats[0])
 	
 			if (len(lats) > 1):
+				_x.append(lats[0])
 				_x.append(lats[1])
-
-			_y.append(lngs[0])
 	
 			if (len(lngs) > 1):
+				_y.append(lngs[0])
 				_y.append(lngs[1])
 
 	x = np.asarray(_x)
 	y = np.asarray(_y)
-	print "x=>"+str(x)
-	print "\ny=>"+str(y)
+	print "x=>"+str(x)+str(len(x))
+	print "\ny=>"+str(y)+str(len(y))
 
-		
-		
-		 
-	
