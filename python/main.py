81a73f4e82cfc60bfd9e6605048f9f220f6562e9
shashank singh <shashank.personal@gmail.com>
2013-01-19 09:29:00 +0530

    some change which git refuses to show me

--- python/main.py
+++ python/main.py
@@ -8,7 +8,7 @@ def run():
 	print "executing"
 	#data=execute(get_all_pickup_drop_lat_lng_timestamp())
 	#data=execute(get_all_pickup_drop_lat_lng_timestamp_moved_origin())
-	data=execute(get_all_pickup_lat_lng_timestamp(limit=90000))
+	data=execute(get_all_pickup_lat_lng_timestamp(limit=90))
 	print len(data)
 	#save_to_backend(data,"file")	
 	x,y,timestamp = convert_to_numpy_x_y_timestamp_singular_data(data)#we are passing pickup data only
