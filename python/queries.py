242b4d7fc0f29d46df7ca1e9f06d4d1e9177c1d9
shashank singh <shashank@olacabs.com>
2013-01-19 09:25:26 +0530

    fixed the warning sending code in databackend py

--- python/queries.py
+++ python/queries.py
@@ -54,6 +54,29 @@ def get_all_pickup_lat_lng_timestamp_moved_origin(limit):
         return """
                 select ob.id,
                 group_concat(nst.status),
+                group_concat(nst.lang - 18.9647) lat,
+                group_concat(nst.lng - 72.8258) lng,
+                group_concat(nst.timestamp) timestamp
+                from
+                        ola_bookings ob
+                left join
+                        ola_citytaxi_status_update nst ON ob.id = nst.booking_id
+                where
+                convert_tz(ob.pickup_date, '+0:00', '+5:30') between '2012-1-1 00:00:00' and '2013-1-15 23:59:59'
+                and ob.status = 'completed'
+                and ob.service_type = 'p2p'
+                and ob.service_city = 'Mumbai'
+                and ob.deleted = 0
+                and nst.status = 5
+                group by ob.id
+                order by nst.status ASC LIMIT 
+                """+str(limit)+""";"""
+
+def get_all_pickup_lat_lng_timestamp(limit):
+        """read the function above , only gets pickup lat,lngalso acc. to google 18.9647 and 72.8258 is center of mumbi"""
+        return """
+                select ob.id,
+                group_concat(nst.status),
                 group_concat(nst.lat) lat,
                 group_concat(nst.lng) lng,
                 group_concat(nst.timestamp) timestamp
