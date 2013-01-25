8e2b69cf900390a5e97782e4cbc6ee08be0dd714
shashank singh <shashank@olacabs.com>
2013-01-26 00:58:15 +0530

    added service city as a arg , also added a simple numerical grid function so as to take help in putting a grid label to coordinates , would be needed in putting time seires in palce

---
+++ python/grid_finder.py
@@ -0,0 +1,8 @@
+i = 0
+for xc in range(188000,195000):
+	for yc in range(727000,732000):
+		xcm = xc/10000.00
+		ycm =  yc/10000.00
+		grid = int(142.857142*(xcm-18.80)+20*(ycm-72.70))
+		print "["+str(i)+"]"+"xc=>"+str(xc)+" yc=>"+str(yc)+" grid=>"+str(grid)
+		i += 1
