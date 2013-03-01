bbcb324283aee17c055b366785dba07d0f4fd216
shashank singh <shashank@olacabs.com>
2013-03-01 17:07:28 +0530

    added two data sources in queries.py and some small changes to learn_svm

--- python/queries.py
+++ python/queries.py
@@ -1,4 +1,4 @@
-
+"""select count(*),date(from_unixtime(timestamp)) as mydate from ola_bookings as ob left join ola_citytaxi_status_update as nst ON ob.id = nst.booking_id  where nst.status = 5 and ob.deleted = "0" and ob.status = 'completed'  and ob.service_type = 'p2p' and ob.service_city = "delhi" and convert_tz(ob.pickup_date, '+0:00', '+5:30') between '2013-1-1 00:00:00' and '2013-1-31 23:59:59' group by mydate """
 def get_all_p2p_metadata_for_hours(hour):
 	"""read all data for each hour from ola_bookings"""
 	return "select p2p_metadata as total_city_taxi_bookings from ola_bookings where service_type='p2p' and year(pickup_date)=2012 and hour(pickup_date)="+hour
@@ -94,4 +94,13 @@ def get_all_pickup_lat_lng_timestamp(limit,service_city):
                 group by ob.id
                 order by timestamp ASC LIMIT 
                 """+str(limit)+""";"""
+
+
+def get_count_of_bookings_paritioned_by_date():
+	"""partition the count of booking date wise"""
+	return """select count(name) as "number of bookings" , date(pickup_date) as date from ola_bookings where pickup_date is not null and pickup_date < now() GROUP BY UNIX_TIMESTAMP(pickup_date) DIV 86400 ;"""
+
+def get_count_of_bookings_paritioned_by_date_by_service_city(service_city):
+	"""partition the count of bookings date wise and from a service city"""
+	return """select count(name) as "number of bookings" , date(pickup_date) as date from ola_bookings where pickup_date is not null and pickup_date < now() and service_city = """.service_city.""" GROUP BY UNIX_TIMESTAMP(pickup_date) DIV 86400 ;"""
 
