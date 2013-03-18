76218252535382f73b42b4b6c381742fda824251
shashank singh <shashank.personal@gmail.com>
2013-03-18 17:17:43 +0530

    did some preprocessing but doesnt look enough

---
+++ sentiment-analyser/preprocess.py
@@ -0,0 +1,20 @@
+import re
+
+def preprocess_tweet(tweets):
+	tweet_processed = []
+	for tweet in tweets:
+		tweet =  tweet.lower()
+		#Convert to lower case
+		tweet = tweet.lower()
+		#Convert www.* or https?://* to URL
+		tweet = re.sub('((www\.[\s]+)|(https?://[^\s]+))','URL',tweet)
+		#Convert @username to AT_USER
+		tweet = re.sub('@[^\s]+','AT_USER',tweet)
+		#Remove additional white spaces
+		tweet = re.sub('[\s]+', ' ', tweet)
+		#Replace #word with word
+		tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+		#trim
+		tweet = tweet.strip('\'"')
+		tweet_processed.append(tweet);
+	return tweet_processed
--- sentiment-analyser/sentiment_analysis.py
+++ sentiment-analyser/sentiment_analysis.py
@@ -4,5 +4,13 @@ from nltk.classify import NaiveBayesClassifier
 from nltk.metrics import BigramAssocMeasures
 from nltk.probability import FreqDist, ConditionalFreqDist
 
-import read_from_twitter
+from read_from_twitter import collated_twitter_data
+from config import SEARCH_OLA_TERMS
+from preprocess import preprocess_tweet
+
+
+tweet = collated_twitter_data(since='',until='',search_terms=SEARCH_OLA_TERMS)
+preprocessed_tweets = preprocess_tweet(tweet)
+
+print preprocessed_tweets
 
