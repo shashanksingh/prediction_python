d7ee74375b54fce8e82c6030db2757b1156de787
shashank singh <shashank.personal@gmail.com>
2013-03-18 19:22:10 +0530

    dataset is now more understood

--- sentiment-analyser/training/read_dataset.py
+++ sentiment-analyser/training/read_dataset.py
@@ -1,2 +1,6 @@
-
 input_file_name = ["debate08_sentiment_tweets.csv" , "github_full_training_dataset.csv"]
+
+#debate time, tweet ,author,nickname rating [1 = negative, 2 = positive, 3 = mixed, 4 = other],
+#debate-->936736034       9/27/08 3:30b sn#debate08 rt @blogdiva NPR is saying their audience gave the debate to Obama.	Populista	Populista	1	3	 2
+#github postive/negative , tweet 
+#github-->"positive","the rock is destined to be the 21st century's new "" conan "" and that he's going to make a splash even greater than arnold schwarzenegger , jean-claud van damme or steven segal ."
