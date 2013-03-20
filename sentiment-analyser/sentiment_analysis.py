a24bb39c3ca5acfe223df89ecd1762bc525d3fb7
shashank singh <shashank@olacabs.com>
2013-03-20 16:13:57 +0530

    pagination added into search twitter api and better keywords added also now timestamp is also sent and what needs to be done is cleanup of data

---
+++ sentiment-analyser/sentiment_analysis.py
@@ -0,0 +1,9 @@
+from read_from_twitter import collated_twitter_data
+from pattern.en import sentiment,positive
+
+
+for page in range (1,5):
+	tweets = collated_twitter_data(search_terms=["#olacabs","@olacabs","#ola cabs"],result_page=page)
+	for tweet in tweets:
+		result = sentiment(tweet["text"])
+		print tweet["timestamp"],"=>",result
