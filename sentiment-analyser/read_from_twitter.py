fc36e3e9cc68b9a33b3c9d417ebebfca0f82f629
shashank singh <shashank@olacabs.com>
2013-03-22 18:51:23 +0530

    some wierd ass plots beign drawn, but atleast they are being drawn

--- sentiment-analyser/read_from_twitter.py
+++ sentiment-analyser/read_from_twitter.py
@@ -23,21 +23,21 @@ def searchTweets(query,page=1,count=30,tweet_type='recent',rate_per_page=100,sin
 	search = urllib.urlopen("http://search.twitter.com/search.json?%s"%str(params))
 	dict = simplejson.loads(search.read())
 	for result in dict["results"]: # result is a list of dictionaries
-		timestamp = time.strftime('%d%m', time.strptime(result["created_at"],'%a, %d %b %Y %H:%M:%S +0000'))
+		timestamp = time.strftime('%y%m%d', time.strptime(result["created_at"],'%a, %d %b %Y %H:%M:%S +0000'))
 		tweets.append({'text':result["text"],'timestamp':timestamp})
 	return tweets
 
 
-def collated_twitter_data(result_since='', result_until='2013-12-12',result_page=0,search_terms=[]):
+def collated_twitter_data(result_since='', result_until='2013-12-12',result_page=0,search_terms=[],count_of_tweets=30):
 	tweets = []
 	for terms in (search_terms):
-		tweets.extend(searchTweets(query=terms,page=result_page,since=result_since,until=result_until))
+		tweets.extend(searchTweets(query=terms,page=result_page,since=result_since,until=result_until,count=count_of_tweets))
 	return tweets
 
 def get_all_olacabs_tweet(page=1,count=30):
 	tweets = []
 	user_timeline = twitter.getUserTimeline(screen_name="olacabs",page=5,count=100)
 	for tweet in user_timeline:
-		timestamp = time.strftime('%d%m', time.strptime(tweet["created_at"],'%a %b %d %H:%M:%S +0000 %Y'))
+		timestamp = time.strftime('%y%m%d', time.strptime(tweet["created_at"],'%a %b %d %H:%M:%S +0000 %Y'))
 		tweets.append({'text':tweet["text"],'timestamp':timestamp})
 	return tweets
