1ecd3f7a294a12b26658874c4deaf9e0eb4c7691
shashank singh <shashank.personal@gmail.com>
2013-03-22 19:49:23 +0530

    atleast thing are being generated correctly

--- sentiment-analyser/sentiment_analysis.py
+++ sentiment-analyser/sentiment_analysis.py
@@ -16,7 +16,7 @@ date_keys_olacabs = [] #stors all the dates data was present for our tweeting
 #	date_array[i]["negative"] = 0.0
 
 #csv priting
-print "Day of This Month ,Sentiment Positive Value, Sentiment Negative Value,Positive(True Or False)"
+#print "Day of This Month ,Sentiment Positive Value, Sentiment Negative Value,Positive(True Or False)"
 for page in range (1,5):
 	tweets_from_others = collated_twitter_data(search_terms=["#olacabs","@olacabs","#ola cabs"],result_page=page,count_of_tweets=100)
 	for tweet in tweets_from_others:
@@ -28,22 +28,23 @@ for page in range (1,5):
 		date_array[str(date_of_data)+"_count"] = date_array.get(str(date_of_data)+"_count",0)+ 1
 		date_keys_others.append(str(date_of_data))
 
-for page in range (1,5):
+for page in range (1,10):
 	all_tweets_from_olacabs = get_all_olacabs_tweet(page,count=100)
 	for tweet in all_tweets_from_olacabs:
 		date_of_data = tweet["timestamp"]
 		date_array[str(date_of_data)+"_count_of_olacabs"] = date_array.get((str(date_of_data)+"_count_of_olacabs"),0) + 1
 		date_keys_olacabs.append(str(date_of_data))
 
-pp.pprint(date_array)
+#pp.pprint(date_array)
+date_keys_others = list(set(date_keys_others)) #remove duplicate
 date_keys_olacabs = list(set(date_keys_olacabs)) #remove duplicates
 
-pp.pprint(date_keys_olacabs)
+#pp.pprint(date_keys_olacabs)
 #for i in range(13,21):
 for i in date_keys_others:
 	if (date_array[str(i)+"_count"] != 0) :
-        	#date_array[str(i)+"_posititve"] /= date_array[str(i)+"_count"]
-        	#date_array[str(i)+"_negative"] /= date_array[str(i)+"_count"]
+        	date_array[str(i)+"_posititve"] /= date_array[str(i)+"_count"]
+        	date_array[str(i)+"_negative"] /= date_array[str(i)+"_count"]
 		pass
 
 positive_sentiment = []
@@ -60,7 +61,7 @@ for i in date_keys_others:
 for i in date_keys_olacabs:
 	total_tweets.append(date_array[str(i)+"_count_of_olacabs"])
 
-print total_tweets
+#print total_tweets
 
 #pp.pprint(date_array)
 
@@ -71,6 +72,9 @@ from pylab import *
 #print t
 #print negative_sentiment
 
+print date_keys_others
+print positive_sentiment
+print negative_sentiment
 figure(num=None, figsize=(14, 6), dpi=80, facecolor='w', edgecolor='k')
 subplot(211)
 positive_sentiment_plot,  =  plot(date_keys_others, positive_sentiment, 'b-o',linewidth = 1.0)
@@ -81,23 +85,23 @@ ylabel('Sentiments #Olacabs')
 title('Sentiment Analysis for 7 day')
 grid(True)
 
-subplot(212)
-count_tweets_plot, = plot(date_keys_others, count,'g-o',linewidth = 1.0)
+#subplot(212)
+#count_tweets_plot, = plot(date_keys_others, count,'g-o',linewidth = 1.0)
 #legend([count_tweets_plot],["#olacabs"])
 #label = "Economist Tweet"
 #plt.annotate(label,xy = (14,10), xytext = (40, 40),textcoords = 'offset points', ha = 'right', va = 'bottom',bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5),arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
 #label = "Ipad Mini Contest"
 #plt.annotate(label,xy = (18,10), xytext = (-20, 20),textcoords = 'offset points', ha = 'right', va = 'bottom',bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5),arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
-xlabel('Day of this month[March]')
-ylabel('Count of tweets ')
-grid(True)
+#xlabel('Day of this month[March]')
+#ylabel('Count of tweets ')
+#grid(True)
 
-subplot(222)
-total_tweets_plot, = plot(date_keys_olacabs, total_tweets, 'k--o' , linewidth = 1.0)
+#subplot(222)
+#total_tweets_plot, = plot(date_keys_olacabs, total_tweets, 'k--o' , linewidth = 1.0)
 #legend([total_tweets_plot],["@Olacabs"])
-xlabel('Day of this month[March]')
-ylabel('Count of tweets ')
-grid(True)
+#xlabel('Day of this month[March]')
+#ylabel('Count of tweets ')
+#grid(True)
 
 savefig("test.png")
 show()
