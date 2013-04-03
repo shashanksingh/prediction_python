d8e2938cc7e9b27275373421af7570aea719ed59
shashank singh <shashank@olacabs.com>
2013-04-03 17:00:54 +0530

    updating

--- php/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-for i in `seq 1 10000`
-do
-	statuscode=`curl http://alerts.olacabs-dev.in/link/crn_to_shortlink/${i} |grep 'HTTP' | awk '{print $2}' `
-	echo "curl http://alerts.olacabs-dev.in/link/crn_to_shortlink/${i}"
-	echo statuscode
-done
